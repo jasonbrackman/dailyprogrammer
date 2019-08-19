a727599e86701359624bfa046f374c5ceebc7378
jasonbrackman <brackman@gmail.com>
2019-08-18 20:59:08 -0700

    wanted to know what would happen if I converted to linear space -- but haven't done a test to see if blending in linear space and converting back is any different yet.

--- challenge_369/easy_hex_colors.py
+++ challenge_369/easy_hex_colors.py
@@ -1,5 +1,6 @@
-from typing import NamedTuple
+from typing import NamedTuple, List
 import re
+import math
 
 # Regex pattern search to be used multiple times
 # Example: #ff0533 -> ff, 05, 33
@@ -10,6 +11,31 @@ class RGB(NamedTuple):
     r: int
     g: int
     b: int
+
+    def linear_srgb(self) -> List[float]:
+        """
+        http://davengrace.com/cgi-bin/cspace.pl
+        :return:
+        """
+        # (n / 255) ^ 2.2 (convert to linear)
+        rl = (self.r / 255) ** 2.2
+        gl = (self.g / 255) ** 2.2
+        bl = (self.b / 255) ** 2.2
+        # print(f"Linear Values: ({rl}, {gl}, {bl})")
+
+        # n ^ 0.4545 (convert to linear sRGB float)
+        rs = rl ** 0.4545
+        gs = gl ** 0.4545
+        bs = bl ** 0.4545
+
+        # final Corrected values
+        values = list()
+        for s in [rs, gs, bs]:
+            values.append(s / 12.92 if s <= 0.04045 else (math.pow((s + 0.055) / 1.055, 2.4)))
+
+        print(f"sRGB Linear Values: ({values[0]}, {values[1]}, {values[2]})")
+
+        return values
 
     def hex(self) -> str:
         red = hex(self.r)
@@ -52,3 +78,17 @@ assert hexcolor(0, 0, 205) == "#0000CD"  # (MediumBlue)
 
 assert blend({"#000000", "#778899"}) == "#3C444C"
 assert blend({"#E6E6FA", "#FF69B4", "#B0C4DE"}) == "#DCB1D9"
+
+# ----- Exploring a bit more ...
+
+# From original sRGB8bit gamma corrected values
+x = RGB(255, 127, 240)
+
+# Convert to linear space
+r = x.linear_srgb()
+
+# Convert back to sRGB gamma corrected
+# -- If I've understood this properly -- which may not be the case.
+test = math.pow(r[1], 1/2.2) * 255
+print(test)
+
