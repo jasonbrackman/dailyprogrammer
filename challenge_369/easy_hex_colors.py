9b1017f02063c7a0c3ee8022992d3d852e30366b
jasonbrackman <brackman@gmail.com>
2019-08-19 23:52:21 -0700

    adding additional functions to handle the gamma corrected blending

--- challenge_369/easy_hex_colors.py
+++ challenge_369/easy_hex_colors.py
@@ -12,36 +12,68 @@ class RGB(NamedTuple):
     g: int
     b: int
 
-    def linear_srgb(self) -> List[float]:
+    @classmethod
+    def from_hex(cls, colour: str):
+        c = re.match(pattern, colour)
+        red = int(c["r"], 16)
+        green = int(c["g"], 16)
+        blue = int(c["b"], 16)
+        return cls(red, green, blue)
+
+    def srgb_to_linear(self) -> List[float]:
         """
         http://davengrace.com/cgi-bin/cspace.pl
         :return:
         """
-        # (n / 255) ^ 2.2 (convert to linear)
-        rl = (self.r / 255) ** 2.2
-        gl = (self.g / 255) ** 2.2
-        bl = (self.b / 255) ** 2.2
-        # print(f"Linear Values: ({rl}, {gl}, {bl})")
-
-        # n ^ 0.4545 (convert to linear sRGB float)
-        rs = rl ** 0.4545
-        gs = gl ** 0.4545
-        bs = bl ** 0.4545
-
-        # final Corrected values
         values = list()
-        for s in [rs, gs, bs]:
-            values.append(s / 12.92 if s <= 0.04045 else (math.pow((s + 0.055) / 1.055, 2.4)))
+        a = 0.055
+        for value in [self.r, self.g, self.b]:
+            value = (value / 255)
+            linear_val_small = value / 12.92
+            linear_val = math.pow(abs((value + a) / (1 + a)), 2.4)
+            values.append(linear_val_small if (value < 0.04045) else linear_val)
 
-        print(f"sRGB Linear Values: ({values[0]}, {values[1]}, {values[2]})")
+        # print(f"Linear Values: ({values[0]}, {values[1]}, {values[2]})")
 
         return values
+
+    @staticmethod
+    def linear_to_srgb(values: List[float]):
+        srgbs = list()
+        a = 0.055
+        for value in values:
+            srgb_val_small = value * 12.92
+            srgb_val = (1+a) * math.pow(abs(value), 1/2.4) - a
+            result = srgb_val_small if (value < 0.0031308) else srgb_val
+
+            srgbs.append(round(result*255))
+
+        # print(f"Perception Values: ({srgbs[0]}, {srgbs[1]}, {srgbs[2]})")
+
+        return RGB(srgbs[0], srgbs[1], srgbs[2])
 
     def hex(self) -> str:
         red = hex(self.r)
         green = hex(self.g)
         blue = hex(self.b)
         return f"#{red[2:].zfill(2)}{green[2:].zfill(2)}{blue[2:].zfill(2)}".upper()
+
+    @staticmethod
+    def blend_hexcodes(colours: set):
+
+        r, g, b = 0.0, 0.0, 0.0
+
+        for colour in colours:
+            a, b, c = RGB.from_hex(colour).srgb_to_linear()
+            r += a
+            g += b
+            b += c
+
+        return RGB.linear_to_srgb([
+            r / len(colours),
+            g / len(colours),
+            b / len(colours)
+        ])
 
     def __repr__(self) -> str:
         hex_conversion = self.hex()
@@ -79,16 +111,15 @@ assert hexcolor(0, 0, 205) == "#0000CD"  # (MediumBlue)
 assert blend({"#000000", "#778899"}) == "#3C444C"
 assert blend({"#E6E6FA", "#FF69B4", "#B0C4DE"}) == "#DCB1D9"
 
-# ----- Exploring a bit more ...
 
-# From original sRGB8bit gamma corrected values
-x = RGB(255, 127, 240)
+# -- Additional Exploration w/ Gamma Correction
+result1 = RGB.blend_hexcodes({"#000000", "#778899"})
+print(f"Gamma Corrected Blend: {result1} and without correction -> #3C444C")
 
-# Convert to linear space
-r = x.linear_srgb()
+result2 = RGB.blend_hexcodes({"#E6E6FA", "#FF69B4", "#B0C4DE"})
+print(f"Gamma Corrected Blend: {result2} and without correction -> #DCB1D9")
 
-# Convert back to sRGB gamma corrected
-# -- If I've understood this properly -- which may not be the case.
-test = math.pow(r[1], 1/2.2) * 255
-print(test)
+result3 = RGB.blend_hexcodes({"#ff0000", "#00ff00"})
+print(result3)
 
+print(blend({"#ff0000", "#00ff00"}))
