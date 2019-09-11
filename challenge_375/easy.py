6213283df59ec0f743555f44d5249b812a0910c8
jasonbrackman <brackman@gmail.com>
2019-09-10 21:22:53 -0700

    Inlined a function to remove overhead. Added profiling info to measure difference between the two solutions.

--- challenge_375/easy.py
+++ challenge_375/easy.py
@@ -14,21 +14,25 @@ def new_number(number: int) -> int:
 
     factor = 1
     while number > 9:
-        result = get_right_most_digit_plus_one(number)
+        result = (number - (number // 10) * 10) + 1
         return_total += result * factor
 
         # Calc values for next loop
         factor = factor * 100 if result == 10 else factor * 10
         number //= 10
 
-    result = get_right_most_digit_plus_one(number)
+    result = (number - (number // 10) * 10) + 1
     return_total += result * factor
 
     return return_total
 
 
-def get_right_most_digit_plus_one(digit: int) -> int:
-    return (digit - (digit // 10) * 10) + 1
+if __name__ == "__main__":
+    import timeit
+    number = 102030405060708090
+    print(timeit.timeit(lambda: new_number_using_strings(number), number=100_000))
+    print(timeit.timeit(lambda: new_number(number), number=100_000))
 
-
-print(new_number(1234))
+    # import cProfile
+    # cProfile.run("timeit.timeit(lambda: new_number_using_strings(102030405060708090), number=1_000)")
+    # cProfile.run("timeit.timeit(lambda: new_number(102030405060708090), number=1_000)")
